4d8cddcb5e5baaa04a6428de16aba965362c94ab
BigManWilli <williamkhuu123@gmail.com>
2025-05-07 14:45:23 +0200

    Dialogue logging conversation

--- Assets/DialogueTrigger.cs
+++ Assets/DialogueTrigger.cs
@@ -7,5 +7,10 @@ public class DialogueTrigger : MonoBehaviour
     // Start is called before the first frame update
     public Dialogue dialogue;
         
+    public void TriggerDialogue() 
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
+    
+}
 
