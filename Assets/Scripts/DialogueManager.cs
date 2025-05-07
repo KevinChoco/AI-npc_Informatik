e08b79c3d42720aef7ad0a9ec683bb87573b87fb
BigManWilli <williamkhuu123@gmail.com>
2025-05-07 15:46:27 +0200

    Animations

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    public Animator animator;
+
     private Queue<string> sentences;
 
     // Start is called before the first frame update
@@ -14,8 +19,11 @@ public class DialogueManager : MonoBehaviour
 
     }
 
-    public void StartDialogue (Dialogue dialogue) {
-        Debug.Log("Starting conversation with " + dialogue.name);
+    public void StartDialogue (Dialogue dialogue) 
+    {
+        animator.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
 
         sentences.Clear();
 
@@ -36,11 +44,12 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        Debug.Log (sentence);
+        dialogueText.text = sentence;
+        
     }
 
     void EndDialogue() {
-        Debug.Log ("End of conversation. ");
+         animator.SetBool("IsOpen", false);
     }
 
 }
